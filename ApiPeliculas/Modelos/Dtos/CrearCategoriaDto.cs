7dd5b53e30b91ae1c34d6a248ca842955aab8b64
DanielJaimes9213 <d.ricardo.j@gmail.com>
2025-04-03 14:08:00 -0600

    CategoriaDTO add

---
+++ ApiPeliculas/Modelos/Dtos/CrearCategoriaDto.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ApiPeliculas.Modelos.Dtos
+{
+    public class CrearCategoriaDto
+    {
+
+        [Required(ErrorMessage = "El nombre es obligatorio")]
+        [MaxLength(100, ErrorMessage = "El nombre no puede tener más de 100 caracteres")]
+        public string Nombre { get; set; }
+    }
+}
