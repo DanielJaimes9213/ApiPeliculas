1bc99120e8e869a86f902938287fc5324d795721
DanielJaimes9213 <d.ricardo.j@gmail.com>
2025-04-03 12:31:05 -0600

    ApplicationDbContext add

---
+++ ApiPeliculas/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using ApiPeliculas.Modelos;
+using Microsoft.EntityFrameworkCore;
+
+namespace ApiPeliculas.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }     
+
+        //Aquí pasar todas las entidades (Modelos)
+        public DbSet<Categoria> Categoria { get; set; }
+    }
+}
