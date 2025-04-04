94fae781f9bcd70f9631518b75928ac8b6c5a206
DanielJaimes9213 <d.ricardo.j@gmail.com>
2025-04-04 00:21:19 -0600

    Pelicula Dtos created

---
+++ ApiPeliculas/Modelos/Dtos/PeliculaDto.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace ApiPeliculas.Modelos.Dtos
+{
+    public class PeliculaDto
+    {
+        public int Id { get; set; }
+
+        public string Nombre { get; set; }
+
+        public string Descripcion { get; set; }
+
+        public int Duracion { get; set; }
+
+        public string RutaImagen { get; set; }
+
+        public enum TipoClasificación { siete, Trece, Dieciseis, Dieciocho }
+
+        public TipoClasificación Clasificación { get; set; }
+
+        public DateTime FechaCreación { get; set; }
+        public int CategoriaId { get; set; }
+
+    }
+}
