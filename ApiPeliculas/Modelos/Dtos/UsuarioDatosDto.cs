0acdff7bb6fd5cb76a23d250d43b8ed66831f958
DanielJaimes9213 <d.ricardo.j@gmail.com>
2025-04-07 14:04:15 -0600

    User's DTOs created

---
+++ ApiPeliculas/Modelos/Dtos/UsuarioDatosDto.cs
@@ -0,0 +1,11 @@
+﻿namespace ApiPeliculas.Modelos.Dtos
+{
+    public class UsuarioDatosDto
+    {
+        public int Id { get; set; }
+
+        public string UserName { get; set; }
+
+        public string Nombre { get; set; }
+    }
+}
---
+++ ApiPeliculas/Modelos/Dtos/UsuarioLoginRespuesta.cs
@@ -0,0 +1,10 @@
+﻿namespace ApiPeliculas.Modelos.Dtos
+{
+    public class UsuarioLoginRespuesta
+    {
+        public UsuarioDatosDto Usuario { get; set; }
+        public string Role { get; set; }
+
+        public string Token { get; set; }
+    }
+}
