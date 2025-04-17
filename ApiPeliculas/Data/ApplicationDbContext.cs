eea0e34bf577f0930902beb21bfe06e04b3cca5c
DanielJaimes9213 <d.ricardo.j@gmail.com>
2025-04-16 22:21:38 -0600

    migration database userIdentity

--- ApiPeliculas/Data/ApplicationDbContext.cs
+++ ApiPeliculas/Data/ApplicationDbContext.cs
@@ -1,14 +1,20 @@
 ﻿using ApiPeliculas.Modelos;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace ApiPeliculas.Data
 {
-    public class ApplicationDbContext : DbContext
+    public class ApplicationDbContext : IdentityDbContext<AppUsuario>
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
         {
 
-        }     
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
 
         //Aquí pasar todas las entidades (Modelos)
         public DbSet<Categoria> Categoria { get; set; }
@@ -16,5 +22,7 @@ namespace ApiPeliculas.Data
         public DbSet<Pelicula> Pelicula { get; set; }
 
         public DbSet<Usuario> Usuario { get; set; }
+
+        public DbSet<AppUsuario> AppUsuario { get; set; }
     }
 }
